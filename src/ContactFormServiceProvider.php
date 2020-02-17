3d54fedc90ee6a4b178fb75b98240f8d7c239986
Vinh Vo <vinh@saritasa.com>
2020-02-17 09:13:14 +0700

    Update package structure

--- src/ContactFormServiceProvider.php
+++ src/ContactFormServiceProvider.php
@@ -7,9 +7,9 @@ use Illuminate\Support\ServiceProvider;
 class ContactFormServiceProvider extends ServiceProvider {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/resources/views', 'contactform');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'contactform');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
 
     public function register()
